76da617d729f67615a5b3484fd2931625e994fc5
m_verhoeven <m_verhoeven@live.nl>
2016-01-05 15:21:58 +0100

    Enemy zone

--- Game Test/PlayerCharacter/Player.cs
+++ Game Test/PlayerCharacter/Player.cs
@@ -295,7 +295,7 @@ namespace Game_Test
                 }
             }
             
-            for (int l = 1; l < layer.Length; l++)
+            for (int l = 2; l < layer.Length; l++)
                 ChangeAlpha(new Vector2(sprite.Position.X + dirX, sprite.Position.Y + dirY), l);
             sprite.Position = new Vector2(sprite.Position.X + dirX, sprite.Position.Y + dirY); //Set new position
             boundingBox.Position = new Vector2(boundingBox.Position.X + dirX, boundingBox.Position.Y + dirY);
